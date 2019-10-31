87d06d553393dc07b047ba609207385903e36cd0
Jordan Matelsky <j6k4m8@gmail.com>
2019-10-31 12:40:06 -0400

    Add a ton of inline docs to FrofPlan and Lark pieces

--- frof/__init__.py
+++ frof/__init__.py
@@ -11,17 +11,76 @@ import uuid
 
 
 class StatusPrinter(abc.ABC):
+    """
+    Abstract class for status-printing capabilities.
+
+    Do not use directly.
+    """
+
     ...
 
     def emit_status(self):
+        """
+        Emit a status for the contained FrofPlan.
+
+        Arguments:
+            None
+
+        Returns:
+            None
+
+        """
         ...
 
 
 class OneLineStatusPrinter(StatusPrinter):
+    """
+    A status printer that keeps itself constrainted to a single line.
+
+    Good for running in a CLI.
+    During a run, prints:
+
+        🤔 7 jobs running, 9 remaining.
+
+    While the run boots, prints:
+
+        Starting job with 17 jobs total.
+
+    When a run has no more remaining jobs, prints:
+
+        👌 0 jobs running, 0 remaining.
+
+    """
+
     def __init__(self, fp: "FrofPlan") -> None:
+        """
+        Create a new OneLineStatusPrinter.
+
+
+
+        Arguments:
+            fp (FrofPlan): The Plan to print for during runs
+
+        Returns:
+            None
+
+        """
         self.fp = fp
 
     def emit_status(self):
+        """
+        Emit the current status of self.fp.
+
+        Prints directly to stdout. Uses emojis. This is not the most backward-
+        compatible of all systems.
+
+        Arguments:
+            None
+
+        Returns:
+            None
+
+        """
         next_job_count = len(self.fp.get_next_jobs())
         if next_job_count:
             emoji = "🤔"
@@ -33,11 +92,47 @@ class OneLineStatusPrinter(StatusPrinter):
         )
 
     def launch_status(self):
+        """
+        Print the current status pre-run of the Plan.
+
+        Arguments:
+            None
+
+        Returns:
+            None
+
+        """
         print(f"Starting job with {len(self.fp.network)} jobs total.", end="\r")
 
 
 class FrofPlan:
+    """
+    FrofPlan objects hold a network of jobs to run, and manage the execution.
+
+    You can invoke this with:
+
+        FrofPlan(FROF_FILE_NAME)
+
+        FrofPlan(FROF_FILE_CONTENTS)
+
+        FrofPlan(my_DiGraph)
+
+    """
+
     def __init__(self, frof, status=OneLineStatusPrinter):
+        """
+        Create a new FrofPlan.
+
+        Arguments:
+            frof (Union[str, nx.DiGraph]): The job network to run. Can be a
+                network, designed manually, or a string representation of a
+                plan, OR the name of a file to read for the plan.
+            status (StatusPrinter): The StatusPrinter to use for this plan.
+
+        Returns:
+            None
+
+        """
         self.plan_id = uuid.uuid4()
         if isinstance(frof, str):
             if "\n" not in frof:
@@ -53,6 +148,16 @@ class FrofPlan:
         self.status = status(self)
 
     def get_next_jobs(self) -> List:
+        """
+        Get a list of the next jobs to run.
+
+        Arguments:
+            None
+
+        Returns:
+            Tuple[str, FrofJob]: (Job Name, Job Object)
+
+        """
         return [
             (i, j["job"])
             for i, j in self.current_network.nodes(data=True)
@@ -60,6 +165,16 @@ class FrofPlan:
         ]
 
     def run(self):
+        """
+        Run the plan.
+
+        Arguments:
+            None
+
+        Returns:
+            None
+
+        """
         run_id = uuid.uuid4()
         self.current_network = copy.deepcopy(self.network)
         self.status.launch_status()
@@ -73,7 +188,7 @@ class FrofPlan:
                             "FROF_BATCH_ITER": str(itercounter),
                             "FROF_JOB_NAME": str(i),
                             "FROF_RUN_ID": run_id,
-                            "FROF_PLAN_ID": self.plan_id
+                            "FROF_PLAN_ID": self.plan_id,
                         }
                     )
                     for itercounter, (i, job) in enumerate(current_jobs)
@@ -85,6 +200,11 @@ class FrofPlan:
             self.status.emit_status()
 
     def print_plan(self):
+        """
+        Print the plan as text.
+
+        Useful for debugging.
+        """
         self.current_network = copy.deepcopy(self.network)
         self.status.launch_status()
         while len(self.current_network):
--- frof/parser/__init__.py
+++ frof/parser/__init__.py
@@ -48,6 +48,12 @@ frof_parser = Lark(SYNTAX)
 
 
 class FrofTransformer(Transformer):
+    """
+    Lark Transformer for Frof syntax.
+
+    You can probably ignore this, unless you're fiddling with the language.
+    """
+
     def __init__(self, *args, **kwargs) -> None:
         self.G = nx.DiGraph()
         self._params = {}
@@ -58,6 +64,9 @@ class FrofTransformer(Transformer):
     def transform(self, tree):
         self._transform_tree(tree)
 
+        # TODO: This needs some serious love. All of this should be happening
+        #       in the FrofPlan domain, not in the language domain. Need to
+        #       figure out how to manage this correctly.
         for (
             jobname,
             (paramname, max_parallel_count),
@@ -66,6 +75,8 @@ class FrofTransformer(Transformer):
             ins = [u for u, v in self.G.in_edges(jobname)]
             outs = [v for u, v in self.G.out_edges(jobname)]
 
+            # TODO: This is bad and wrong. Pretty much everything below this
+            #       line needs to go.
             if max_parallel_count is None:
                 for option in self._params[paramname]:
                     job_text = job["job"].cmd
@@ -81,6 +92,16 @@ class FrofTransformer(Transformer):
                         self.G.add_edge(f"{jobname}_{option}", i)
                 self.G.remove_node(jobname)
             else:
+                # TODO: Very bad. This effectively groups each set of jobs into
+                #       batches of size `max_parallel_count` and then schedules
+                #       a NullJob() in between them so that each batch has to
+                #       finish before the next can begin. (You can picture it
+                #       like a fan_out to max_parallel_count and then an
+                #       immediate fan-back-in to n=1 at the NullJob sentries.)
+                #       This is bad because it prevents other jobs from running
+                #       while their peers are still going. i.e. all of jobs
+                #       1,2,3,4 must finish running before 5 can start, even if
+                #       2 has already finished and there's a spare "slot".
                 max_parallel_count = int(max_parallel_count)
                 options = self._params[paramname]
                 option_groups = [
@@ -161,10 +182,36 @@ class FrofTransformer(Transformer):
 
 
 class FrofParser:
+    """
+    A parser for the .frof syntax.
+
+    Contains minimal logic; see FrofTransformer for more details.
+    """
+
     def __init__(self) -> None:
+        """
+        Create a new Parser.
+
+        Arguments:
+            None
+
+        Returns:
+            None
+
+        """
         pass
 
     def parse(self, frof: str) -> nx.DiGraph:
+        """
+        Parse a .frof syntax tree.
+
+        Arguments:
+            frof (str): The contents to parse
+
+        Return:
+            nx.DiGraph: The parsed job network
+
+        """
         tree = frof_parser.parse(frof)
         G = FrofTransformer().transform(tree)
         return G
