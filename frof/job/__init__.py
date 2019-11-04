9761205c816f7b7391d4d8f63112c6f33ec3c8cc
Jordan Matelsky <j6k4m8@gmail.com>
2019-11-04 17:22:05 -0500

    Add BashJob docs

--- frof/job/__init__.py
+++ frof/job/__init__.py
@@ -6,26 +6,71 @@ class Job:
 
 
 class BashJob(Job):
+    """
+    BashJobs manage the execution of simple commands in a local shell.
+
+    These are executed in the current working directory.
+    """
+
     def __init__(self, cmd: str, use_env_vars=True, env=None) -> None:
+        """
+        Create a new BashJob.
+
+        Arguments:
+            cmd (str): The command to execute
+            use_env_vars (bool: True): Whether to set environment variables
+            env (dict: None): Custom environment variables to use
+
+        Returns:
+            None
+
+        """
         self.cmd = cmd
         self.use_env_vars = use_env_vars
         self.env = env if env else {}
 
     async def run(self, env_vars=None):
+        """
+        Run the command.
+
+        Arguments:
+            env_vars (dict: None): Custom environment variables to use
+
+        Returns:
+            None
+
+        """
         cmd = self.cmd
         env = {}
         if self.use_env_vars:
             env = {**env_vars, **self.env}
+
+        # Cast all env-vars to string (int/float other types are not supported
+        # by Python's subprocess module).
         env = {k: str(v) for k, v in env.items()}
 
         process = await asyncio.create_subprocess_shell(cmd, env=env)
         _ = await process.communicate()
 
     def __str__(self) -> str:
+        """
+        Produce this BashJob as a string.
+
+        Returns:
+            str: A human-readable string
+
+        """
         return f"<{self.cmd[:10]}>"
 
     def __repr__(self) -> str:
-        return f"<{self.cmd[:10]}>"
+        """
+        Produce this BashJob as a string.
+
+        Returns:
+            str: A human-readable string
+
+        """
+        return f"BashJob('{self.cmd}', env={self.env})"
 
 
 class NullJob(Job):
