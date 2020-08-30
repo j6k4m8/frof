2d846c7edb9c4f2c8a0499c20286e737f82e5d95
Jordan Matelsky <j6k4m8@gmail.com>
2020-08-30 12:30:22 -0400

    Add slurm to global exports

--- frof/__init__.py
+++ frof/__init__.py
@@ -1,4 +1,4 @@
 from .parser import FrofParser
 from .statusmonitor import NullStatusMonitor
-from .executor import LocalFrofExecutor
+from .executor import LocalFrofExecutor, SlurmFrofExecutor
 from .version import __version__
